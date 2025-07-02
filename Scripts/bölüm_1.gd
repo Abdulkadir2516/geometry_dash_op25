54c1aff9b2a5809839204f6fcefa2b3dd76264ad
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-02 21:32:50 +0300

    yer yüzü yapıldı

--- Scripts/bölüm_1.gd
+++ Scripts/bölüm_1.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-var hiz = 10
+var hiz = 5
 var bg1 = Sprite2D
 var bg2 = Sprite2D
-
+var g1 = Sprite2D
+var g2 = Sprite2D
 func _ready() -> void:
 	bg1 = $"Bg04-hd"
 	bg2 = $"Bg04-hd2"
+	g1=$ground1
+	g2=$ground2
 
 func _physics_process(delta: float) -> void:
 	bg1.position.x -= hiz
@@ -16,3 +19,11 @@ func _physics_process(delta: float) -> void:
 		
 	elif bg2.position.x < -600:
 		bg2.position.x = 1790
+		
+	g1.position.x -= hiz*3
+	g2.position.x -= hiz*3
+	if g1.position.x < -600:
+		g1.position.x = 1790
+		
+	elif g2.position.x < -600:
+		g2.position.x = 1790
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,25 @@
 extends CharacterBody2D
 
 @export var karakterHiz = 7
-
+var kontrol = true
 #Birinci Yöntem
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
 	if Input.is_action_just_pressed("jump"):
 		
-		for i in range(10):
-			velocity.y = velocity.y - 1
-			move_and_collide(velocity * karakterHiz)
-			await get_tree().create_timer(.05).timeout
-		
-		for i in range(10):
-			velocity.y += 1
-			move_and_collide(velocity * karakterHiz)
-			await get_tree().create_timer(.05).timeout
-		
+		if kontrol:
+			kontrol = false
+			for i in range(10):
+				velocity.y = velocity.y - 2
+				move_and_collide(velocity * karakterHiz)
+				await get_tree().create_timer(.03).timeout
+			
+			for i in range(10):
+				velocity.y += 2
+				move_and_collide(velocity * karakterHiz)
+				await get_tree().create_timer(.03).timeout
+			kontrol = true
+			
 	#move_and_collide(velocity * karakterHiz)
 
 
