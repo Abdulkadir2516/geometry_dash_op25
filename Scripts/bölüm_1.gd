33e991ac7282c3b2fee75b51df4c554f33a02b80
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-23 20:58:54 +0300

    güncelleme

--- Scripts/bölüm_1.gd
+++ Scripts/bölüm_1.gd
@@ -16,9 +16,13 @@ func _ready() -> void:
 	for i in range(1,8):
 		
 		var yeni_karakter = $blok.duplicate()
-		yeni_karakter.position = Vector2(i*600,400)
+		yeni_karakter.position = Vector2(1000 + i*60,400)
 		yeni_karakter.name = "blok" + str(i)
 		add_child(yeni_karakter)
+	
+	
+	
+	
 
 	
 
@@ -40,6 +44,7 @@ func _physics_process(delta: float) -> void:
 	#elif g2.position.x < -600:
 		#g2.position.x = 1790
 		
+		
 	_sabit_hızda_hareket(g1, hiz*3)
 	_sabit_hızda_hareket(g2, hiz*3)
 	_sabit_hızda_hareket(bg1, hiz)
@@ -47,7 +52,7 @@ func _physics_process(delta: float) -> void:
 	
 	for i in range(1,8):
 		
-		_sabit_hızda_hareket2(get_node("blok"+str(i)), hiz*2)
+		_sabit_hızda_hareket2(get_node("blok"+str(i)), hiz*1.5)
 	
 	
 func _sabit_hızda_hareket(karakter:Sprite2D, hiz:float):
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,8 +4,6 @@ extends CharacterBody2D
 var kontrol = true
 #Birinci Yöntem
 func _physics_process(delta: float) -> void:
-
-
 	velocity = Vector2.ZERO
 	if Input.is_action_just_pressed("jump"):
 		
@@ -13,31 +11,25 @@ func _physics_process(delta: float) -> void:
 			kontrol = false
 			for i in range(10):
 				velocity.y = velocity.y - 2
-				$".".rotation += 18 
 				move_and_collide(velocity * karakterHiz)
 				await get_tree().create_timer(.04).timeout
 			
-			for i in range(10):
-				velocity.y += 2
-				$".".rotation += 18 
-				move_and_collide(velocity * karakterHiz)
-				await get_tree().create_timer(.04).timeout
+			
+				
 			kontrol = true
 			
-		$".".rotation = 0 
+		$".".rotation = 0
+		
+	if position.y < 430 and not Input.is_action_just_pressed("jump") and kontrol:
+		velocity.y += 2
+		move_and_collide(velocity * karakterHiz)
+		await get_tree().create_timer(.1).timeout
+
 		
 		
-		
- 
 	
 	#move_and_collide(velocity * karakterHiz)
 
-
-
-	
-	
-	
-		
 
 # İkinci Yöntem
 #func _physics_process(delta: float) -> void: 
